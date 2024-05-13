6efff3dd3da8fa3e0e81d85840a73e1f378d2e79
ntando-mv15 <ntando.mv15@gmail.com>
2024-05-13 22:36:36 +0200

    Updated Resume with details

--- resume-site/scripts.js
+++ resume-site/scripts.js
@@ -1,4 +1,6 @@
 const counter = document.querySelector(".counter-number");
+
+// Function to update counter
 async function updateCounter() {
     let response = await fetch(
         "https://p2znc5c5ien66ffypmaganbb2i0nbynn.lambda-url.us-east-1.on.aws/"
@@ -6,4 +8,7 @@ async function updateCounter() {
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`;
 }
+
+
+// Update counter on page load
 updateCounter();
